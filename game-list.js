9ee4dd0a0653d479558fd42f031adfca74c06d8b
mohamedbkkl <83860073+mohamedbkkl@users.noreply.github.com>
2024-08-06 17:22:38 +0100

    Memory

---
+++ game-list.js
@@ -0,0 +1,32 @@
+const games = [
+    {name : "Memory Game For Kids", icon : "6.png", link : "https://play.google.com/store/apps/details?id=com.ZeppelinGames.MemoryGameForKids"},
+    {name : "Kids Coloring And Drawing", icon : "5.png", link : "https://play.google.com/store/apps/details?id=com.ZeppelinGames.Coloring"},
+    {name : "House Builder For Kids", icon : "4.png", link : "https://play.google.com/store/apps/details?id=com.ZeppelinGames.HouseBuilderForKids"},
+    {name : "Kids Puzzles Game", icon : "3.png", link : "https://play.google.com/store/apps/details?id=com.ZeppelinGames.KidsPuzzlesGame"},
+    {name : "Find The Differences", icon : "2.png", link : "https://play.google.com/store/apps/details?id=com.zeppelingames.findthedifference"},
+    {name : "Math Kids Puzzle", icon : "1.png", link : "https://play.google.com/store/apps/details?id=com.zeppelingames.mathkidspuzzle"},
+];
+
+function createGameHTML(game) {
+    return `
+        <div>
+            <a class="game" href="${game.link}" target="_blank">
+                <img class="gameIcon" src="${game.icon}">
+                <h1>${game.name}</h1>
+                <img class="googleplay" src="google-play-store-png.png">
+            </a>
+        </div>
+     `;
+}
+
+function initGameList(containerId) {
+    const gameContainer = document.getElementById(containerId);
+    if (gameContainer) {
+        const gameListHTML = games.map(createGameHTML).join('');
+        gameContainer.innerHTML = gameListHTML;
+    } else {
+        console.error(`Could not find element with id '${containerId}'`);
+    }
+}
+
+window.initGameList = initGameList;
